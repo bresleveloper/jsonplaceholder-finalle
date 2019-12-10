9b6859ef43334acb2f80654a2a6ab954f8a65280
bresleveloper <ariel.rubi@gmail.com>
2019-12-10 16:58:45 +0200

    docs

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # LaFinnale
+
+
+`ng build --prod --base-href="https://bresleveloper.github.com/jsonplaceholder-finalle/" --outputPath=/docs`
--- src/app/services/base.service.ts
+++ src/app/services/base.service.ts
@@ -2,9 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
 export class BaseService<T> {
 
 
